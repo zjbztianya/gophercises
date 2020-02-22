abb0c6189db1f399c2f87cbf9122eacdb79c678b
zjbztianya <spcjv5@gmail.com>
2020-02-22 17:10:48 +0800

    done quiz game

--- quiz/main.go
+++ quiz/main.go
@@ -6,18 +6,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type Problem struct {
-	question string
-	answer   string
+	text   string
+	answer string
 }
 type Problems []Problem
 
-func main() {
-	csvFileName := flag.String("csv", "./quiz/problems.csv", "a csv file in the format of 'question,answer'")
-	flag.Parse()
-
+func loadProblems(csvFileName *string) Problems {
 	csvFile, err := os.Open(*csvFileName)
 	if err != nil {
 		log.Fatalf("open %s fail\n", *csvFileName)
@@ -28,9 +26,48 @@ func main() {
 		log.Fatalln("read problems fail")
 	}
 
-	problems := make(Problems, len(lines))
+	problems := make(Problems, 0, len(lines))
 	for _, s := range lines {
-		problems = append(problems, Problem{question: s[0], answer: s[1]})
-		fmt.Println(s[0],s[1])
+		problems = append(problems, Problem{text: s[0], answer: s[1]})
 	}
+
+	return problems
+}
+
+func starQuiz(problem Problem, correct chan int) {
+	var answer string
+	fmt.Println(problem.text + "?")
+	fmt.Scanln(&answer)
+	if answer == problem.answer {
+		fmt.Println("correct!")
+		correct <- 1
+	} else {
+		fmt.Println("wrong!")
+		correct <- 0
+	}
+}
+
+func run(problems Problems) (correct int) {
+	quizTimer := time.NewTimer(5 * time.Second)
+	correctChan := make(chan int, 1)
+	for _, problem := range problems {
+		go starQuiz(problem, correctChan)
+		select {
+		case <-quizTimer.C:
+			fmt.Println("timeout!")
+			return correct
+		case result := <-correctChan:
+			correct += result
+		}
+		quizTimer.Reset(5 * time.Second)
+	}
+	fmt.Println("quiz end!")
+	return
+}
+
+func main() {
+	csvFileName := flag.String("csv", "./quiz/problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	fmt.Printf("right answers number:%d\n", run(loadProblems(csvFileName)))
 }
